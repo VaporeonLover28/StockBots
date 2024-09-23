6b6d0766c8d08d959d5cb9040949abcd31f531e3
Mia <vitoria.wirz23@nave.org.br>
2024-09-23 16:27:55 -0300

    Credits scene
    adicionei a tela de credito porém precisa de melhorias ainda

--- Menus/menu.gd
+++ Menus/menu.gd
@@ -20,7 +20,8 @@ func _on_start_btn_pressed():
 	start()
 
 func _on_credits_btn_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://Menus/credits_scene.tscn")
+
 
 func _on_quit_btn_pressed():
 	get_tree().quit()
