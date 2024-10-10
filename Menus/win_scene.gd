66f31475b83a061c64d077c8ed7d5be3d387a8c6
VaporeonLover28 <walter.sobreira23@nave.org.br>
2024-10-10 08:19:51 -0300

    idk
    sometig

--- Menus/win_scene.gd
+++ Menus/win_scene.gd
@@ -1,5 +1,7 @@
 extends CanvasLayer
 
+var secret_pressed = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
@@ -7,6 +9,9 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("Especial"):
+		secret_pressed = true
+	
+	if secret_pressed == true:
 		$Back_menu_button.text = "Next victim"
 		$Pizza.animation = "eye"
 		$Label.text = "Do not belive in their lies, stupid robot."
@@ -21,7 +26,11 @@ func _process(delta: float) -> void:
 func _on_back_menu_button_pressed() -> void:
 	RoundCounter.rounds = 0 
 	PlayerLoadout.times_passed_item = 0
-	PlayerLoadout.newest_weapon = RoundCounter.current_item_weapons.pick_random()
-	PlayerLoadout.oldest_weapon = null
+	if secret_pressed == false:
+		PlayerLoadout.newest_weapon = RoundCounter.current_item_weapons.pick_random()
+		PlayerLoadout.oldest_weapon = null
+	else:
+		PlayerLoadout.newest_weapon = load("res://Weapons/secret.tres")
+		PlayerLoadout.oldest_weapon = load("res://Weapons/secret.tres")
 	PlayerLoadout.current_mode = load("res://Modes/basic.tres")
 	get_tree().change_scene_to_file("res://Area_Batalha.tscn")
